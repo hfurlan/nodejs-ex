14d8dc3f55703fc725bd0fecc46644c2aa3ab1a8
Hildebrando Furlan Neto <hildebrando.furlan@gmail.com>
2018-11-18 19:34:17 -0200

    Melhorias na tela OnLine

--- server.js
+++ server.js
@@ -119,6 +119,10 @@ app.get('/', function (req, res) {
   res.render('index.html');
 });
 
+app.get('/login', function (req, res) {
+  res.render('login.html');
+});
+
 app.post('/biometria', function (req, res) {
   if (!db) {
     initDb(function(err){});
